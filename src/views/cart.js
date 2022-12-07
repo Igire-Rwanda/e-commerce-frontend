43efc41f30a77cb88307c386fb8d36cd9c2f893e
mutuyimana1 <amutuyimana64@gmail.com>
2022-12-07 10:38:02 +0200

    chng

--- src/views/cart.js
+++ src/views/cart.js
@@ -1,103 +1,93 @@
-// import React, { useState } from "react";
-// import { Button, Container, Col, Row, Table } from "react-bootstrap";
-// import { useCart } from "react-use-cart";
-// import "../views/cart.css";
+import React, { useState } from "react";
+import { Button, Container, Col, Row, Table } from "react-bootstrap";
+import { useCart } from "react-use-cart";
+import "../views/cart.css";
 
-// const Cart = () => {
-//   const {
-//     isEmpty,
-//     items,
-//     removeItem,
-//     cartTotal,
-//     updateItemQuantity,
-//     emptyCart,
-//   } = useCart();
+const Cart = () => {
+  const {
+    isEmpty,
+    items,
+    removeItem,
+    cartTotal,
+    updateItemQuantity,
+    emptyCart,
+  } = useCart();
 
-//   const [price, setPrice] = useState(0);
+  const [price, setPrice] = useState(0);
 
-//   return (
-//     <div className="t">
-//       <Container className="">
-//         <h1>{isEmpty ? "The Cart is Empty" : "Here Is Your Products"}</h1>
-//       </Container>
-//       <Row className="row">
-//         <Table striped bordered hover variant="dark" className="table">
-//           <tbody>
-//             {items.map((item, index) => {
-//               return (
-//                 <tr key={index}>
-//                   <td className="image">
-//                     <div className="photo">
-//                       <img src={item.image} />{" "}
-//                     </div>
-//                   </td>
-//                   <div className="name">
-//                     <td className="productname">{item.productName}</td>
-//                   </div>
+  return (
+    <div className="t">
+      <Container className="">
+        <h1>{isEmpty ? "The Cart is Empty" : "Here Is Your Products"}</h1>
+      </Container>
+      <Row className="row">
+        <Table striped bordered hover variant="dark" className="table">
+          <tbody>
+            {items.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td className="image">
+                    <div className="photo">
+                      <img src={item.image} />{" "}
+                    </div>
+                  </td>
+                  <div className="name">
+                    <td className="productname">{item.productName}</td>
+                  </div>
 
-//                   <div>
-//                     <td className="price">{item.price}</td>
-//                   </div>
+                  <div>
+                    <td className="price">{item.price}</td>
+                  </div>
 
-//                   <div className="table2">
-//                     <button
-//                       className="q1"
-//                       onClick={() =>
-//                         updateItemQuantity(item.id, item.quantity - 1)
-//                       }
-//                     >
-//                       -
-//                     </button>
-//                    <button className="quantity">{item.quantity}</button>
-//                    <button
-//                       className="q2"
-//                       onClick={() =>
-//                         updateItemQuantity(item.id, item.quantity + 1)
-//                       }
-//                     >
-//                       +
-//                     </button>
+                  <div className="table2">
+                    <button
+                      className="q1"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity - 1)
+                      }
+                    >
+                      -
+                    </button>
+                    <button className="quantity">{item.quantity}</button>
+                    <button
+                      className="q2"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity + 1)
+                      }
+                    >
+                      +
+                    </button>
 
-//                     <button
-//                       className="danger"
-//                       variant="danger"
-//                       onClick={() => removeItem(item.id)}
-//                     >
-//                       RemoveItem
-//                     </Button>
+                    <button
+                      className="danger"
+                      variant="danger"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      RemoveItem
+                    </button>
+                  </div>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+        {!isEmpty && (
+          <Row>
+            <Col>
+              <h4 className="total">Total Price: Rwf .{price}</h4>
+            </Col>
+            <Col>
+              <Button className="cart" onClick={() => emptyCart()}>
+                Clear Cart
+              </Button>
 
-//                   </div>
-//                 </tr>
-//               );
-//             })}
-//           </tbody>
-//         </Table>
-//         {!isEmpty && (
-//           <Row>
-//             <Col>
-//               <h4 className="total">Total Price: Rwf .{price}</h4>
-//             </Col>
-//             <Col>
-//               <Button className="cart" onClick={() => emptyCart()}>
-//                 Clear Cart
-//               </Button>
+              <Button className="chec">Proceed to Chekout</Button>
+            </Col>
+          </Row>
+        )}
+      </Row>
+    </div>
+  );
+};
 
-//               <Button className="chec">
-//                 Proceed to Chekout
-//               </Button>
-//             </Col>
-//           </Row>
-//         )}
-//       </Row>
-//       <Button
-//                       className="danger"
-//                       variant="danger"
-
-//                     >
-//                       <a href="/checkout">Buy Now</a>
-//                     </Button>
-//     </div>
-//   );
-// };
-
-// export default Cart;
+export default Cart;
